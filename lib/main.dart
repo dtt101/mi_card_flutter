3ea20439e1f0a5a3c06b7495e365573c8c24578b
David Thompson <dtt101@gmail.com>
2019-06-05 15:45:37 +0100

    Added avatar

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 50.0,
+                backgroundImage: AssetImage('images/profile-photo.jpg'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
